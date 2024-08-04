574d35e75b084259d5e4694e3cce33c007a3441c
khandare.v <khandare.v@media.net>
2024-08-04 17:39:41 +0530

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,12 +85,95 @@ export default function TabsDemo() {
           <TabsList className="grid w-full grid-cols-4">
             <TabsTrigger value="podsnameparse">Pods Name Parser</TabsTrigger>
             <TabsTrigger value="sanitychecker">Sanity Checker</TabsTrigger>
+            <TabsTrigger value="links">Links</TabsTrigger>
           </TabsList>
           <TabsContent value="podsnameparse">
-            <PodsNameParser/>
+            <PodsNameParser />
           </TabsContent>
           <TabsContent value="sanitychecker">
             <iframe src="https://vaibhavkhandare.github.io/Sanity_checker/"></iframe>
+          </TabsContent>
+          <TabsContent value="links">
+            <div className="flex items-center justify-center h-[1g000px] bg-gray-100">
+              <div className="bg-white p-8 rounded shadow-lg">
+                <ol className="relative border-s border-gray-200 dark:border-gray-700 w-[400px]">
+                  <li className="mb-10 ms-6">
+                    <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                      <svg
+                        className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                      </svg>
+                    </span>
+                    <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                      <a
+                        href="http://tree.mn/Video-AdExchange/ad-exchange/-/tags"
+                        target="_blank"
+                      >
+                        Create New Tag
+                      </a>
+                    </h3>
+                    <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                      Create a new Tag, oldTag + 1
+                    </p>
+                  </li>
+                  <li className="mb-10 ms-6">
+                    <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                      <svg
+                        className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                      </svg>
+                    </span>
+                    <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                      <a
+                        href="http://ci.internal.media.net/job/mowx/job/mowx-deploy-pipeline/"
+                        target="_blank"
+                      >
+                        Deploy Pipeline
+                      </a>
+                    </h3>
+                    <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                      
+                      Deploy the Tag
+                      
+                    </p>
+                  </li>
+                  <li className="mb-10 ms-6">
+                    <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                      <svg
+                        className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                      </svg>
+                    </span>
+                    <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                      <a
+                        href="http://ci.internal.media.net/job/ProgrammaticBuying/job/ssx/job/EBDA/job/mowx-sanity-x/"
+                        target="_blank"
+                      >
+                        Sanity X
+                      </a>
+                    </h3>
+                    <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                      Check Tag on Sanity
+                    </p>
+                  </li>
+                </ol>
+              </div>
+            </div>
           </TabsContent>
         </Tabs>
       </TabsContent>
